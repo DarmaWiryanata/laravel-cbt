d98f845574d77f268be8432a857c2cd7d5d0bf8d
Darma Wiryanata <45613994+ScrawnyGus@users.noreply.github.com>
2020-05-18 12:58:01 +0800

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Schema::defaultStringLength(191);
+        setlocale(LC_ALL, 'id_ID.utf8');
+        \Carbon\Carbon::setLocale('id_ID.utf8');
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
